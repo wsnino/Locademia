daf024f1f7c37a44822d9bcf3446267888183016
marcelo pereira <wsnino.contato@gmail.com>
2021-03-14 13:23:17 -0300

    criando estrurura das paginas cabeçalho,rodape e menu

---
+++ src/br/com/softblue/loucademia/Config.java
@@ -0,0 +1,11 @@
+package br.com.softblue.loucademia;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+import javax.faces.annotation.FacesConfig.Version;
+
+@ApplicationScoped
+@FacesConfig(version = Version.JSF_2_3)
+public class Config {
+
+}
